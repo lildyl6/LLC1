d9b05adbf225f6aa57dde96227b063e28c660408
Eric Martin <martin.eric313@gmail.com>
2024-01-24 12:01:45 -0500

    Testing

--- app/src/main/java/com/example/llc1/MainActivity.kt
+++ app/src/main/java/com/example/llc1/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LLC1Theme {
-        Greeting("Android")
+        Greeting("dylan aadkasf")
     }
 }
