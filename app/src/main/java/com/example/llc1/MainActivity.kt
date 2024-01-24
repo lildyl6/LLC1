eaeca4cf24afe45b5d9d7f83d613ab566bc239a6
Eric Martin <martin.eric313@gmail.com>
2024-01-24 14:12:33 -0500

    confirm button

--- app/src/main/java/com/example/llc1/MainActivity.kt
+++ app/src/main/java/com/example/llc1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.llc1
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.example.llc1.model.Model
 
@@ -13,5 +14,7 @@ class MainActivity : ComponentActivity(){
         setContentView(R.layout.main_activity)
 
         model = Model(0, 0, 0, 0, 0)
+        var val1 : Int = 0
+        val edit1 = findViewById<EditText>(R.id.edit1)
     }
 }
