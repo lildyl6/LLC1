e9c55bb0543066ce4b9e88f3412fd52d6934119c
dylanjones <4dylan.jone@gmail.com>
2024-01-24 12:48:11 -0500

    first commit

--- app/src/main/java/com/example/llc1/MainActivity.kt
+++ app/src/main/java/com/example/llc1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.llc1
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val b = findViewById<Button>(R.id.button)
+        b.setOnClickListener{
+            finish()
+        }
     }
 }
 
